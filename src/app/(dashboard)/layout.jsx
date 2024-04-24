366dc7e58cf6fd2da079dfec3c159748c713f668
Pok Hengly <henglypok21012021@gmail.com>
2024-04-24 14:45:43 +0700

    add code

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -18,6 +18,7 @@ export default function RootLayout({children}) {
                 <div className="w-full ml-10">
                     <NavbarComponent/>
                     <TodoCardComponent/>
+
                 </div>
             </main>
 
